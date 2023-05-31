7088ffe1845e29cf6621081181d3f2da54c42dcf
Kenn ~PC <nacionkenn@gmail.com>
2023-06-01 02:00:45 +0800

    Added login buttons

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginForm extends StatelessWidget {
@@ -19,6 +20,8 @@ class LoginForm extends StatelessWidget {
           _CreateAccount(),
           SizedBox(height: 32),
           _EmailAndPasswordForms(),
+          SizedBox(height: 32),
+          _FormButtons(),
         ],
       ),
     );
@@ -117,16 +120,57 @@ class _EmailAndPasswordForms extends StatelessWidget {
           decoration: InputDecoration(
             border: OutlineInputBorder(borderRadius: BorderRadius.zero),
           ),
-
         ),
         const SizedBox(height: 12),
         Center(
           child: RichText(
-            text:  TextSpan(
-              text: 'Forgot Password?',
-              style: const TextStyle(decoration: TextDecoration.underline, color: Colors.black),
-              recognizer: TapGestureRecognizer()..onTap = () => log('Forgot Password is tapped', name: 'Forgot password? ')
-            ),
+            text: TextSpan(
+                text: 'Forgot Password?',
+                style: const TextStyle(decoration: TextDecoration.underline, color: Colors.black),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () => log('Forgot Password is tapped', name: 'Forgot password? ')),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _FormButtons extends StatelessWidget {
+  const _FormButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            padding: const EdgeInsets.symmetric(vertical: 24),
+          ),
+          child: Text(
+            'Sign In',
+            style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Colors.white),
+          ),
+        ),
+        const SizedBox(height: 12),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey,
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            padding: const EdgeInsets.symmetric(vertical: 24),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(FontAwesomeIcons.google, color: Colors.black,),
+              SizedBox(width: 8),
+              Text('Sign In with Google ', style: Theme.of(context).textTheme.labelSmall,),
+            ],
           ),
         )
       ],
